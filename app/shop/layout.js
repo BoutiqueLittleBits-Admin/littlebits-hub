defca5ffffa00507e374ccba52f73e852ee5653b
BoutiqueLittleBits-Admin <msholleeann@gmail.com>
2026-01-18 15:01:26 -0600

    Create layout.js

---
+++ app/shop/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Shop | Boutique Little Bits',
+  description: 'Browse our curated collection of stickers, accessories, gift boxes, stationery and more. Find the perfect gift or treat yourself!',
+}
+
+export default function ShopLayout({ children }) {
+  return children;
+}
