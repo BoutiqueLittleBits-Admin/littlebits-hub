3889ea9b6135549191f650647d4eee9f7fd41461
BoutiqueLittleBits-Admin <msholleeann@gmail.com>
2026-01-18 14:51:19 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./app/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          sage: '#5F7F73',
+          mint: '#8FAE9A',
+          coral: '#D7746B',
+          blush: '#E8A3B3',
+          cream: '#FAF8F5',
+          gold: '#E9C36A',
+          slate: '#5C7F9B',
+        }
+      }
+    },
   },
   plugins: [],
 }
