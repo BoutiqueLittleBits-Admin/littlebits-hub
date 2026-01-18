59ac550b01ac36697b00f676643372289415f1b2
BoutiqueLittleBits-Admin <msholleeann@gmail.com>
2026-01-18 14:38:39 -0600

    Create CartContext.js

---
+++ app/components/CartContext.js
@@ -0,0 +1,34 @@
+"use client";
+import { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (index) => {
+    setCartItems(cartItems.filter((_, i) => i !== index));
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const cartCount = cartItems.length;
+
+  const cartTotal = cartItems.reduce((sum, item) => sum + parseFloat(item.price), 0).toFixed(2);
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart, cartCount, cartTotal }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
