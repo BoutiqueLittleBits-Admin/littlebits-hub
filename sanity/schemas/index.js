4debe95e8ea1b15db2b7c64b4c6859c17c51765e
BoutiqueLittleBits-Admin <msholleeann@gmail.com>
2026-01-21 00:15:21 -0600

    Update index.js

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,4 +1,4 @@
-import product from './product';
-import category from './category';
+import product from './product.js';
+import category from './category.js';
 
 export const schemaTypes = [product, category];
