3c5a55ea20b4c6c8eee260644650ee1d82f3208a
BoutiqueLittleBits-Admin <msholleeann@gmail.com>
2026-01-20 20:15:12 -0600

    Create sanity.config.js

---
+++ sanity.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './sanity/schemas';
+
+export default defineConfig({
+  name: 'boutique-little-bits',
+  title: 'Boutique Little Bits',
+  
+  projectId: 'y8kpuglx',
+  dataset: 'production',
+  
+  basePath: '/studio',
+  
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+  
+  schema: {
+    types: schemaTypes,
+  },
+});
